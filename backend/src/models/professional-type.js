4010e16ef2058be525a66c1dac9ee83629d6a532
Eduardo Marinho <eduardommmarinho@gmail.com>
2022-06-27 22:46:25 -0300

    feat: defines ProfessionalType entity

---
+++ backend/src/models/professional-type.js
@@ -0,0 +1,38 @@
+'use strict';
+import { Model } from 'sequelize';
+export default (sequelize, DataTypes) => {
+  class ProfessionalType extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      models.ProfessionalType.hasMany(models.Professional, {
+        as: 'professionals',
+        foreignKey: 'professionalTypeId'
+      });
+    }
+  }
+
+  ProfessionalType.init(
+    {
+      description: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: { notEmpty: true }
+      },
+      situation: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: true
+      }
+    },
+    {
+      sequelize,
+      underscored: true
+    }
+  );
+
+  return ProfessionalType;
+};
