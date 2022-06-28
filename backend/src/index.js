bdabcf3e3336f31f802033f8ffcda5b5765964e5
Eduardo Marinho <eduardommmarinho@gmail.com>
2022-06-27 22:32:32 -0300

    feat: imports libraries and middlewares
    This functions and objects are used widely in the project

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,2 +1,16 @@
 import dotenv from 'dotenv';
 dotenv.config();
+
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+
+import routes from './routes';
+import {
+  appErrorHandler,
+  genericErrorHandler,
+  notFound
+} from './middlewares/error.middleware';
+import logger, { logStream } from './config/logger';
+
+import morgan from 'morgan';
