6d871632fc6f181ab94768f955928d7c345efda9
Eduardo Marinho <eduardommmarinho@gmail.com>
2022-06-27 22:40:08 -0300

    feat: imports libraries and configurations

--- backend/src/config/database/index.js
+++ backend/src/config/database/index.js
@@ -1,2 +1,7 @@
+import Sequelize from 'sequelize';
+import * as sequelizeOptions from '../../../.sequelizerc';
+import { Umzug, SequelizeStorage } from 'umzug';
+import logger from '../logger';
+
 import dotenv from 'dotenv';
 dotenv.config();
