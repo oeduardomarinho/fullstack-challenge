c128fca56e5cd5f559c46c693d6c3d88dc0ac8ac
Eduardo Marinho <eduardommmarinho@gmail.com>
2022-06-27 22:43:28 -0300

    feat: creates config file with environment variables

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -1,1 +1,28 @@
 require('dotenv').config();
+
+module.exports = {
+  development: {
+    username: process.env.USERNAME_DATABASE,
+    password: process.env.PASSWORD_DATABASE,
+    database: process.env.DATABASE,
+    host: process.env.HOST_DATABASE,
+    port: process.env.PORT_DATABASE,
+    dialect: process.env.DIALECT_DATABASE
+  },
+  test: {
+    username: process.env.USERNAME_DATABASE_TEST,
+    password: process.env.PASSWORD_DATABASE_TEST,
+    database: process.env.DATABASE_DATABASE_TEST,
+    host: process.env.HOST_DATABASE_TEST,
+    port: process.env.PORT_DATABASE_TEST,
+    dialect: process.env.DIALECT_DATABASE_TEST
+  },
+  production: {
+    username: process.env.USERNAME_DATABASE,
+    password: process.env.PASSWORD_DATABASE,
+    database: process.env.DATABASE,
+    host: process.env.HOST_DATABASE,
+    port: process.env.PORT_DATABASE,
+    dialect: process.env.DIALECT_DATABASE
+  }
+};
