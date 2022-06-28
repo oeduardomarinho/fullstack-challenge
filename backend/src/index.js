22eb7d8ed6d3c638c3a01aa0082487b0bc775be6
Eduardo Marinho <eduardommmarinho@gmail.com>
2022-06-27 22:36:43 -0300

    feat: instanciates express and set middlewares
    These middlewares are third party and it are responsibles over some HTTP
    headers and the logging of the application

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,6 +15,13 @@ import logger, { logStream } from './config/logger';
 
 import morgan from 'morgan';
 
+const app = express();
 const host = process.env.APP_HOST;
 const port = process.env.APP_PORT;
 const api_version = process.env.API_VERSION;
+
+app.use(cors());
+app.use(helmet());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(morgan('combined', { stream: logStream }));
