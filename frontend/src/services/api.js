9ae73dd648b51db2079f53cb7f6d5efd35af6a27
Eduardo Marinho <eduardommmarinho@gmail.com>
2022-07-04 22:22:19 -0300

    fix: remove missed console.log

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -4,7 +4,6 @@ const host = process.env.REACT_APP_API_HOST;
 const port = process.env.REACT_APP_API_PORT;
 const api_version = process.env.REACT_APP_API_VERSION;
 
-console.log(`${host}:${port}/api/${api_version}`)
 const api = axios.create({
   baseURL: `${host}:${port}/api/${api_version}`,
   headers: {
